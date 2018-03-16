97879e6165f815a9e066c635ae71cd98a02816e6
KeriPMiller <miller.keripatricia@gmail.com>
2018-03-16 16:50:34 -0400

    house keepping

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,19 +3,19 @@ module.exports = {
     title: `Keri Can Code`,
   },
   plugins: [`gatsby-plugin-react-helmet`, {
-    resolve: `gatsby-plugin-typography`,
-    options: {
-      pathToConfigModule: `src/utils/typography.js`,
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
     },
-  },
-  {
-    resolve: `gatsby-plugin-google-fonts`,
-    options: {
-      fonts: [
-        `Monoton`,
-        `source sans pro\:400` // you can also specify font weights and styles
-      ]
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Monoton`,
+          `source sans pro\:400` // you can also specify font weights and styles
+        ]
+      }
     }
-  }
-],
+  ],
 };
