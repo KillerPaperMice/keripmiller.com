3e2fc187f9dbf6298de2c87505dbba91b3b97f3d
KeriPMiller <miller.keripatricia@gmail.com>
2018-03-15 22:53:17 -0400

    adding function and form to project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import SingleProject from './SingleProject';
 
-const Projects = () => (
-  <div style={{
+const Projects = () => (<div style={{
     display: `flex`,
     flexDirection: `column`,
-    borderLeft: `10px solid rgb(200, 69, 14)`,
+    borderLeft: `10px solid rgb(200, 69, 14)`
   }}>
-    <h3>Projects</h3>
-    <SingleProject/>
-  </div>
-)
+  <h3>Projects</h3>
+  <SingleProject
+    title={`Whiskr`}
+    projectLink= {`https://whiskrapp.herokuapp.com`}
+    tech={`React.js, Node.js, Express,js, Sass, Heroku`}
+    description={`Senior Capstone project. A Tinder-style pet adoption app that connects users with pets in shelters using Petfinder's API.`}/>
+
+  <SingleProject
+    title={`Whiskr`}
+    projectLink= {`https://whiskrapp.herokuapp.com`}
+    tech={`React.js, Node.js, Express,js, Sass, Heroku`}
+    description={`Senior Capstone project. A Tinder-style pet adoption app that connects users with pets in shelters using Petfinder's API.`}/>
+
+  <SingleProject
+    title={`Whiskr`}
+    projectLink= {`https://whiskrapp.herokuapp.com`}
+    tech={`React.js, Node.js, Express,js, Sass, Heroku`}
+    description={`Senior Capstone project. A Tinder-style pet adoption app that connects users with pets in shelters using Petfinder's API.`}/>
+</div>)
 
 export default Projects;
--- src/components/SingleProject.jsx
+++ src/components/SingleProject.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Link from 'gatsby-link';
 import styles from '../layouts/SingleProject.module.css'
+import assets from '../utils/assets/placeholder.jpg'
 
-const SingleProject = () => (
-  <div className={styles.singleProjectContainer}>
-    <p>test</p>
-  </div>
-)
+class SingleProject extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    console.log(this.props)
+    return (<div className={styles.singleProjectContainer}>
+      <img className={styles.img} src={assets} alt='Whiskr App'/>
+      <h4>{this.props.title}</h4>
+      <p className={styles.techStack}>Tech Stack: {this.props.tech}</p>
+      <p className={styles.projectDescripion}>{this.props.description}</p>
+    </div>)
+  }
+}
 
 export default SingleProject;
