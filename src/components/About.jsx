be28716b304cac6410e63dbf769c45af75be5c90
KeriPMiller <miller.keripatricia@gmail.com>
2018-03-18 20:56:34 -0400

    removed ipsum

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,10 +9,10 @@ const About = () => (
   <div className={styles.storyContainer}>
     <h3>From Art to Tech</h3>
     <p className={styles.story}>
-      Spicy jalapeno bacon ipsum dolor amet pig bresaola boudin meatloaf corned beef sirloin buffalo. Tongue boudin tail kevin pancetta ground round. Burgdoggen cow tail leberkas boudin. Prosciutto strip steak kevin ground round, beef shank landjaeger rump. Venison rump drumstick leberkas ham t-bone, kevin buffalo swine tenderloin prosciutto pancetta.
+      I'm a New York City Developer with a passion for tech and art.
     </p>
     <p className={styles.story}>
-      Porchetta chuck pork chop swine flank picanha ribeye buffalo short loin sirloin drumstick. Pork pig tail leberkas ham hock chicken. Kielbasa biltong ribeye, ball tip tongue spare ribs rump. Burgdoggen pancetta porchetta swine pastrami ribeye pork belly kevin. Spare ribs ball tip pig leberkas jerky tongue pancetta. Doner andouille boudin, filet mignon landjaeger spare ribs picanha capicola salami sausage turducken rump hamburger meatball. Prosciutto tail frankfurter doner burgdoggen jerky meatloaf pastrami capicola buffalo flank meatball t-bone picanha sirloin.
+      After college I had some spare time on my hands and wanted to make a portfolio website. I attend a beginner's html/css meetup with Women Who Code and during that class they had a surprise guest speaker that gave a lecture about creating art with Processing. I absolutely loved it. I've always thought of programming as just this utilitarian tool, I never had thought about it as a form of art itself. I branched out from HTML and CSS , started self teaching and going to other meetups. I got into Fullstacks Academy's Grace Hopper Program and here I am today.
     </p>
   </div>
   <img className={styles.profile} src={profile} alt='profile picture'/>
