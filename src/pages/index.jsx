c7e96df33893204359dc0eb35a441df12f43c66b
KeriPMiller <miller.keripatricia@gmail.com>
2018-03-20 18:43:18 -0400

    added favicon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Helmet} from 'react-helmet';
 import Link from 'gatsby-link';
+import favIco from '../utils/favicon.png'
 // components
 import About from '../components/About';
 import Projects from '../components/Projects';
@@ -12,6 +13,10 @@ const IndexPage = () => (<div>
   <Helmet>
     // font awesome CDN
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"></link>
+
+    <link rel="shortcut icon" type="image/png" href={favIco}/>
+
+
   </Helmet>
 
   <About/>
