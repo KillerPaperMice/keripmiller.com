d9054a01c439df823b174abe31d86bffe7c95d20
KeriPMiller <miller.keripatricia@gmail.com>
2018-03-20 15:56:06 -0400

    added project description overlay, project links and details

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,38 +1,45 @@
-import React from 'react';
-import SingleProject from './SingleProject';
+import React from "react";
+import SingleProject from "./SingleProject";
 
-import whiskrImg from '../utils/assets/screenShots/WhiskrLanding.png';
-import cageImg from '../utils/assets/screenShots/CageMatchCart.png';
-import finalMeltImg from '../utils/assets/screenShots/FinalMeltdownLanding.png'
+// sceen shots
+import whiskrImg from "../utils/assets/screenShots/WhiskrLanding.png";
+import cageImg from "../utils/assets/screenShots/CageMatchCart.png";
+import finalMeltImg from "../utils/assets/screenShots/FinalMeltdownLanding.png";
 
-const Projects = () => (<div style={{
-    display: `flex`,
-    flexDirection: `column`,
-    borderLeft: `10px solid rgb(200, 69, 14)`,
-    marginBottom: `2rem`
+const Projects = () => (
+  <div
+    style={{
+      display: `flex`,
+      flexDirection: `column`,
+      borderLeft: `10px solid rgb(200, 69, 14)`,
+      marginBottom: `2rem`
+    }}
+  >
+    <h3>Projects</h3>
+    <SingleProject
+      title={`Whiskr`}
+      img={whiskrImg}
+      projectLink={`https://whiskrapp.herokuapp.com`}
+      tech={`React.js, Node.js, Express,js, Sass, Heroku`}
+      description={`Senior Capstone project. A Tinder-style pet adoption app that connects users with pets in shelters using Petfinder's API.`}
+    />
 
-  }}>
-  <h3>Projects</h3>
-  <SingleProject
-    title={`Whiskr`}
-    img={whiskrImg}
-    projectLink= {`https://whiskrapp.herokuapp.com`}
-    tech={`React.js, Node.js, Express,js, Sass, Heroku`}
-    description={`Senior Capstone project. A Tinder-style pet adoption app that connects users with pets in shelters using Petfinder's API.`}/>
+    <SingleProject
+      title={`The Final Meltdown`}
+      img={finalMeltImg}
+      projectLink={`https:thefinalmeltdown.github.io/theFinalMeltdown/`}
+      tech={`A-frame, VR, 3D, Open Source`}
+      description={`An open source VR app that uses A-frame to create an symbolic representation of the effects of climate change on the planet.`}
+    />
 
-  <SingleProject
-    title={`The Final Meltdown`}
-    img={finalMeltImg}
-    projectLink= {`https:thefinalmeltdown.github.io/theFinalMeltdown/`}
-    tech={`A-frame, VR, 3D, Open Source`}
-    description={`Senior Capstone project. A Tinder-style pet adoption app that connects users with pets in shelters using Petfinder's API.`}/>
-
-  <SingleProject
-    title={`CageMatch`}
-    img={cageImg}
-    projectLink= {`http://cage-match.herokuapp.com/`}
-    tech={`React.js, Node.js, Express,js, Sass, Heroku, React Notifications`}
-    description={`Senior Capstone project. A Tinder-style pet adoption app that connects users with pets in shelters using Petfinder's API.`}/>
-</div>)
+    <SingleProject
+      title={`CageMatch`}
+      img={cageImg}
+      projectLink={`http://cage-match.herokuapp.com/`}
+      tech={`React.js, Node.js, Express,js, Sass, Heroku, React Notifications`}
+      description={`An e-commerce site for purchasing Nicholas Cage roles.`}
+    />
+  </div>
+);
 
 export default Projects;
--- src/components/SingleProject.jsx
+++ src/components/SingleProject.jsx
@@ -1,20 +1,35 @@
-import React, {Component} from 'react';
-import Link from 'gatsby-link';
-import styles from '../layouts/SingleProject.module.css'
+import React, { Component } from "react";
+import Link from "gatsby-link";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faCode from "@fortawesome/fontawesome-free-solid/faCode";
+import styles from "../layouts/SingleProject.module.css";
 
 class SingleProject extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   render() {
-    console.log(this.props)
-    return (<div className={styles.singleProjectContainer}>
-      <img className={styles.img} src={this.props.img} alt='Whiskr App'/>
-      <p className={styles.proTitle}>{this.props.title}</p>
-      <p className={styles.techStack}>Tech Stack: {this.props.tech}</p>
-      {/* <p className={styles.projectDescripion}>{this.props.description}</p>*/}
-    </div>)
+    console.log(this.props);
+    return (
+      <div className={styles.singleProjectContainer}>
+        <div className={styles.imgContainer}>
+          <img className={styles.img} src={this.props.img} alt="Whiskr App" />
+          <div className={styles.overlay}>
+            <div className={styles.projectDescripion}>
+              {this.props.description}
+            </div>
+          </div>
+        </div>
+        <p className={styles.proTitle}>
+          {this.props.title}{" "}
+          <a href={this.props.projectLink} target='_blank'>
+            <FontAwesomeIcon icon={faCode} />
+          </a>
+        </p>
+        <p className={styles.techStack}>Tech Stack: {this.props.tech}</p>
+      </div>
+    );
   }
 }
 
