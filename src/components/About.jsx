379a72f959697f68df628dfc21ae172180104f87
KeriPMiller <miller.keripatricia@gmail.com>
2018-03-20 16:11:48 -0400

    added dev story

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import styles from '../layouts/About.module.css';
-import profile from '../utils/assets/keriYello.png';
+import React from "react";
+import styles from "../layouts/About.module.css";
+import profile from "../utils/assets/keriYello.png";
 
 console.log(styles);
 
-const About = () => (
-  <div className={styles.about}>
+const About = () => (<div className={styles.about}>
   <div className={styles.storyContainer}>
     <h3>From Art to Tech</h3>
     <p className={styles.story}>
-      I'm a New York City Developer with a passion for tech and art.
+      I'm a New York City developer with a passion for tech and art. I love making creative and fun projects that either make my life easier or happier.
     </p>
     <p className={styles.story}>
-      After college I had some spare time on my hands and wanted to make a portfolio website. I attend a beginner's html/css meetup with Women Who Code and during that class they had a surprise guest speaker that gave a lecture about creating art with Processing. I absolutely loved it. I've always thought of programming as just this utilitarian tool, I never had thought about it as a form of art itself. I branched out from HTML and CSS , started self teaching and going to other meetups. I got into Fullstacks Academy's Grace Hopper Program and here I am today.
+      I feel in love with coding after a surprise guest speaker gave a lecturer at a meetup about creating art with Processing. I absolutely loved it. I've always had thought of programming as just this utilitarian tool, I never had thought about it as a form of art itself. I started self teaching and going to other meetups. In 2017, I graduated Fullstack Academy's Grace Hopper Program and here I am today!
     </p>
   </div>
-  <img className={styles.profile} src={profile} alt='profile picture'/>
-</div>)
+  <img className={styles.profile} src={profile} alt="profile picture"/>
+</div>);
 
 export default About;
