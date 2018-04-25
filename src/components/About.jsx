08a3d7ed8f82ae0c69a2db8821932bf2f4680d73
KeriPMiller <miller.keripatricia@gmail.com>
2018-04-25 15:39:21 -0400

    updated blurb

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,11 +14,11 @@ const About = () => (
         happier.
       </p>
       <p className={styles.story}>
-        I fell in love with coding after a surprise guest speaker gave a
-        lecturer at a meetup about creating art with Processing. I absolutely
+        I fell in love with coding after a guest
+        lecture at a meetup about creating art with Processing. I absolutely
         loved it. I've always had thought of programming as just this
         utilitarian tool, I never had thought about it as a form of art itself.
-        I started self teaching and going to other meetups. In 2017, I graduated
+        I started self teaching and going to other programming meetups. In 2017, I graduated
         Fullstack Academy's Grace Hopper Program and here I am today!
       </p>
     </div>
